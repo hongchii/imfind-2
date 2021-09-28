ee6f39e5f4b38ba9487f9a6b8f45c39b5f3f0fed
hongchii <flyh0321@gmail.com>
2021-09-28 13:24:49 +0900

    마리아디비 연결 , 연결 테스트

---
+++ src/test/java/MariaDBConnectionTest.java
@@ -0,0 +1,26 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import org.apache.log4j.Logger;
+import org.junit.Test;
+
+public class MariaDBConnectionTest {
+
+	private static final Logger log = Logger.getLogger(MariaDBConnectionTest.class);
+	String url = "jdbc:mariadb://localhost:3306/imfind?user=root&password=1234";
+
+	@Test
+	public void connectionTest() {
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			Connection connection = DriverManager.getConnection(url);
+			log.info("### connection : " + connection + " - 연결 성공 ####");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
